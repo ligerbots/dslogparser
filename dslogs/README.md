9f2429e69980f9979c83f1a4e584d6752b3520bd
Paul Rensing <prensing@ligerbots.org>
2019-12-28 12:48:37 -0500

    Fix README and setup of dslogs

--- dslogs/README.md
+++ dslogs/README.md
@@ -1,3 +1,3 @@
 # dslogs
 
-dslogs contains the DSlogs class which is useful for streaming log entries to "real time" application. See package dslogparser.xf
+dslogs contains the DSlogs class which is useful for streaming log entries to "real time" application. See package dslogparser.
--- dslogs/setup.py
+++ dslogs/setup.py
@@ -51,8 +51,7 @@ setup(
     install_requires=['arrow', 'dslogparser'],
     keywords=['FRC', 'logfiles'],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'License :: OSI Approved :: MIT License',
